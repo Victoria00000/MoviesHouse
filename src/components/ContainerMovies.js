feedbb01797e70df07df5611ca7ec959fa168402
Victoria00000 <vvictoriaandres@gmail.com>
2022-02-15 16:15:25 -0300

    arreglando el fetch

--- src/components/ContainerMovies.js
+++ src/components/ContainerMovies.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import css from './ContainerMovies.module.css';
 import { CardMovies } from './CardMovies';
 
-
 export const ContainerMovies = () => {
     
     const [data, setdata] = useState([]);
@@ -15,8 +14,8 @@ export const ContainerMovies = () => {
                     "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYjJlN2Q0ODgxZWZhODE4Y2RmOGY3M2E3MGMwNmZhOCIsInN1YiI6IjYyMGFhNzhkYTMxM2I4MDBiNWNmYjc0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8szRIxcTL_b77Cn976wq3O2rfP89TCOlfkPAL3x6SYc",
                 "Content-Type": "application/json;charset=utf-8",
             },
-        }).then(rta => {rta.json(); console.log(rta);})
-            .then(rtas => setdata(rtas.results));
+        }).then(rta => {console.log(rta); return rta.json();})
+             .then(rtas => setdata(rtas.results));
     }, []);
 
     return (
