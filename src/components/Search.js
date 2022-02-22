59d8e1f0448f156823806c8aeb6f35fd45951c2c
Victoria00000 <vvictoriaandres@gmail.com>
2022-02-22 11:35:38 -0300

    Agregando el Search, a medio camino: useNavigate + cambios visuales

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,24 @@
+import css from './Search.module.css';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { ImSearch } from 'react-icons/im';
+
 export const Search = () => {
+  const [InputValue, setInputValue] = useState('');
+  const navigate = useNavigate();
+
+  const Submit = evt => {
+    evt.preventDefault();
+    navigate('/?search=' + InputValue);
+  };
+  const setValue = evt => setInputValue(evt.target.value);
+
   return (
-    <div>SEARCH</div>
+    <div className={css.divForm}>
+      <form className={css.form} onSubmit={Submit}>
+        <input type='text' className={css.input} value={InputValue} onChange={setValue} />
+        <button type='submit' className={css.button}> <ImSearch className={css.icon} size={20} /> </button>
+      </form>
+    </div>
   )
-}
+};
