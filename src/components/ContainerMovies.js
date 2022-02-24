4b976fbdfe7f7b06784271530578d1247a180a97
Victoria00000 <vvictoriaandres@gmail.com>
2022-02-24 17:30:36 -0300

    comenzando el scroll infinito con: react-infinite-scroll

--- src/components/ContainerMovies.js
+++ src/components/ContainerMovies.js
@@ -4,12 +4,14 @@ import css from './ContainerMovies.module.css';
 import { CardMovies } from './CardMovies';
 import { Fetch } from './Fetch';
 import { LoadingDesing } from './LoadingDesing';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function useQuery() { return new URLSearchParams(useLocation().search) };
 
 export const ContainerMovies = () => {
     const [data, setData] = useState([]);
     const [Loading, setLoading] = useState(true);
+    const [Page, setPage] = useState(1);
 
     const query = useQuery();
     const search = query.get('search');
@@ -21,16 +23,20 @@ export const ContainerMovies = () => {
             setLoading(false);
             return (setData(rtas.results));
         });
-    }, [search]);
+    }, [search, Page]);
 
     if (Loading) { return <LoadingDesing />; };
 
+    const Next = () => setPage(prevPage => prevPage + 1);
+
     return (
-        <ul className={css.ContainerMovies}>
-            {data.map((obj) => {
-                return (<CardMovies key={obj.id} obj={obj} />)
-            })}
-        </ul>
+        <InfiniteScroll dataLength={data.length} hasMore={true} next={Next} >
+            <ul className={css.ContainerMovies}>
+                {data.map((obj) => {
+                    return (<CardMovies key={obj.id} obj={obj} />)
+                })}
+            </ul>
+        </InfiniteScroll>
     )
 };
 
