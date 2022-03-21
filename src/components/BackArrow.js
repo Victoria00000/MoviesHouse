18619b60a121a42d2984c6f9684e93225923015a
Victoria00000 <vvictoriaandres@gmail.com>
2022-03-21 06:43:19 -0300

    limpiando

--- src/components/BackArrow.js
+++ src/components/BackArrow.js
@@ -2,11 +2,12 @@ import { BsFillArrowUpSquareFill } from 'react-icons/bs';
 import css from './BackArrow.module.css';
 
 export const BackArrow = () => {
-    const back = () => window.scrollTo({top:0, behavior:'smooth'});
+    const back = () => window.scrollTo({ top: 0, behavior: 'smooth' });
 
     return (
         <>
-            <BsFillArrowUpSquareFill size={30} className={css.backButton} onClick={back} />
+            <BsFillArrowUpSquareFill size={30} className={css.backButton}
+                id='arrow' onClick={back} />
         </>
     );
 };
